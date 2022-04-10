85005fd1d26ca1ec2bd13a7b16477327396ecdd6
Lim Chee Aun <cheeaun@gmail.com>
2022-04-10 13:53:25 +0800

    Use a different test snapshot

--- utils/testSnapshot.js
+++ utils/testSnapshot.js
@@ -1,4 +1,5 @@
-import docs from '../test-snapshots/complex-radar-crash-20200613.json';
+// import docs from '../test-snapshots/complex-radar-crash-20200613.json';
+import docs from '../test-snapshots/complex-radar-crash-20210417.json';
 
 export default () => ({
   empty: false,
